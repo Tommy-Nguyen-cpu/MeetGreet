f8dc1a5f8891a4eebb4e06e2e638fe44b2491e3f
Tommy-Nguyen <75864631+Tommy-Nguyen-cpu@users.noreply.github.com>
2023-03-11 15:33:36 -0500

    Migrating From .NET Framework To .NET Core
    Moving our project from a .NET Framework project to a .NET Core project.
    
    Massive difference, .NET Core provides more functionality/features.

---
+++ Models/Addresses.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace MeetGreet2.Models
+{
+    public class Addresses
+    {
+        public List<AddressInfo> elements { get; set; }
+    }
+    public class AddressInfo
+    {
+        public double lat { get; set; }
+        public double lon { get;set; }
+        public center center { get; set; }
+        public tags tags { get; set; }
+    }
+
+    public class center
+    {
+        public double lat { get; set; }
+        public double lon { get; set; }
+    }
+
+    public class tags
+    {
+        public string name { get; set; }
+    }
+}
