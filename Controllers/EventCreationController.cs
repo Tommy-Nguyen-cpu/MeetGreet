7f090803d63f8dc9a1312a451172eb78ed90b314
hoytbatwit <54991313+hoytbatwit@users.noreply.github.com>
2023-03-16 15:50:22 -0400

    pushing event creation

---
+++ Controllers/EventCreationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeetGreet.Controllers
+{
+    public class EventCreationController : Controller
+    {
+        public IActionResult EventCreationPage()
+        {
+            return View();
+        }
+    }
+}
