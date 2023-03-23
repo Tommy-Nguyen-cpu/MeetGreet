a88d9a8b257f19a99ea81670192fa113f5529d43
hoytbatwit <54991313+hoytbatwit@users.noreply.github.com>
2023-03-22 23:12:37 -0400

    working on getting map api to work

--- Controllers/EventSubmitController.cs
+++ Controllers/EventSubmitController.cs
@@ -1,5 +1,6 @@
 ﻿using MeetGreet.Models;
 using Microsoft.AspNetCore.Mvc;
+using SendGrid;
 
 namespace MeetGreet.Controllers
 {
@@ -11,7 +12,7 @@ namespace MeetGreet.Controllers
         }
 
         [HttpPost]
-        public IActionResult EventSubmitPage(String userEventName, string userDescription, string userAddress, string userCity, string userZipCode)
+        public async Task<IActionResult> EventSubmitPage(string userEventName, string userDescription, string userAddress, string userCity, string userZipCode)
         {
             Event userEvent = new Event
             {
@@ -21,7 +22,19 @@ namespace MeetGreet.Controllers
                 city = userCity,
                 zipcode = userZipCode
             };
-            return View(userEvent);
+            
+            HttpClient client = new HttpClient();
+
+            //HttpResponseMessage response = await client.GetAsync("https://overpass-api.de/api/interpreter?data=[out:json];area[name=%22" + userAddress + "%22];out%20center%20;");
+            //HttpResponseMessage response = await client.GetAsync("https://overpass-api.de/api/interpreter?data=[out:json];area[addr:housenumber=%22101%22, addr:street=%22Larchmont Lane%22, addr:postcode=%2202339%22, addr:city=%22Hanover%22];out%20center%20;");
+            //HttpResponseMessage response = await client.GetAsync("https://overpass-api.de/api/interpreter?data=[out:json];area[addr:full=%22" + housenumber + userAddress + userZipCode + userCity + "%22];out%20center%20;");
+            //HttpResponseMessage response = await client.GetAsync("https://overpass-api.de/api/interpreter?data=[out:json];node[addr:full=%22101 Larchmont Lane 02339 Hanover%22];out%20center%20;");
+            HttpResponseMessage response = await client.GetAsync("https://overpass-api.de/api/interpreter?data=[out:json];area[name=%22Hanover%22]; area[addr:housenumber=%22101%22]; area[addr:street=%22Larchmont Lane%22]; area[addr:postcode=%2202339%22];out%20center%20;");
+            var eventMarker = await response.Content.ReadFromJsonAsync<Addresses>();
+
+            ViewData["EventMarker"] = eventMarker;
+            ViewData["Event"] = userEvent;
+            return View();
         }
     }
 }
