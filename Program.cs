aca1a412d197ef9e3d51935c5cadbded281d0dc2
Tommy-Nguyen <75864631+Tommy-Nguyen-cpu@users.noreply.github.com>
2023-03-25 13:28:49 -0400

    Simplified Default Page Setting
    "[Authorize]" already does the work of redirecting us to login page. So the previous commits for default page being login is unnecessary.
    
    "AccountController" and "/Account/Login.cshtml" (Non-Identity folder) is not necessary so I removed them.

--- Program.cs
+++ Program.cs
@@ -42,7 +42,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapRazorPages();
 
