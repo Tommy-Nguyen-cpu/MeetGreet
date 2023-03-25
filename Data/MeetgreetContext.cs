ea6c20d48d22e0dffe6a1ee961fde0eff5419905
Tommy-Nguyen <75864631+Tommy-Nguyen-cpu@users.noreply.github.com>
2023-03-24 20:11:36 -0400

    Change Connection String

--- Data/MeetgreetContext.cs
+++ Data/MeetgreetContext.cs
@@ -29,7 +29,7 @@ public partial class MeetgreetContext : IdentityUserContext<User, string, Usercl
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;user=root;database=MEETGREET;password=MakotoNaegi2023", ServerVersion.Parse("8.0.31-mysql"));
+        => optionsBuilder.UseMySql("server=localhost;user=root;database=MEETGREET;password={YOUR PASSWORD}", ServerVersion.Parse("8.0.31-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
