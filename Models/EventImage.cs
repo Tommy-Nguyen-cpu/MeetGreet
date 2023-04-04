dd66549528ab847419f408528fb7ed96f07defd5
Gabriel Baffo <baffog@wit.edu>
2023-04-04 01:02:59 -0400

    Added local support for images to event creation (no S3 support).

---
+++ Models/EventImage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Drawing;
+
+namespace MeetGreet.Models
+{
+	public partial class EventImage
+	{
+        public IFormFile imageFileForm { get; set; } = null!;
+        public byte[] imageBytes { get; set; } = null!;
+    }
+}
+
