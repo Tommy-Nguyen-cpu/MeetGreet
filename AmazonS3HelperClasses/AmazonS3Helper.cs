fcee56ba85d12144eae54e7fc24dbb8628c3c5a6
Gabriel Baffo <baffog@wit.edu>
2023-04-06 12:06:51 -0400

    Added AmazonS3Helper class.

--- AmazonS3HelperClasses/AmazonS3Helper.cs
+++ AmazonS3HelperClasses/AmazonS3Helper.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections;
+using System.Security.AccessControl;
 using Amazon.S3;
 using Amazon.S3.Model;
 using MeetGreet.Data;
+using MeetGreet.Models;
 using MySqlConnector;
 
 namespace MeetGreet.AmazonS3HelperClasses
@@ -22,13 +25,99 @@ namespace MeetGreet.AmazonS3HelperClasses
 			this.connect = connect;
 
             this.s3Client = instantiateS3Client(connect);
-
 		}
 
-
-        public static void uploadImageAndSaveToDatabase(string bucketName, string objectName, string filePath, byte[] imageBytes)
+        // Returns Object Name
+        public async Task<string> uploadImageToS3Bucket(byte[] imageBytes, Event userEvent)
         {
-            // TODO: Reorganize Code
+            return await uploadEventImages(Convert.ToBase64String(imageBytes), userEvent);
+        }
+
+
+
+        private string generateObjectName()
+        {
+            return DateTime.Now.ToString("yyMMdd") + DateTime.Now.ToString("HH:mm:ss") + "-" + Guid.NewGuid().ToString();
+        }
+
+        private async Task<string> uploadEventImages(string byteString, Event userEvent)
+        {
+            if(connect.State == System.Data.ConnectionState.Open)
+            {
+                Console.WriteLine("The connection state is already open.");
+            }
+            else {
+                connect.Open();
+            }
+
+            // GET S3 CLIENT FROM SQL
+
+            // Sends a request for API KEYS FOR AWS
+            MySqlCommand command = new MySqlCommand("SELECT * FROM AWSAPIKey WHERE ID=1", connect);
+
+            IAmazonS3 s3Client = new AmazonS3Client();
+
+            // Reads result.
+            MySqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                s3Client = new AmazonS3Client(reader.GetValue(1).ToString(), reader.GetValue(2).ToString(), Amazon.RegionEndpoint.USEast1);
+            }
+
+            reader.Close();
+
+            string objectName = generateObjectName();
+            byte[] byteArray = Convert.FromBase64String(byteString);
+
+            if (await UploadFileAsync(s3Client, BUCKET_NAME, objectName, "", byteArray))
+            {
+                //saveImageToDatabase(userEvent, objectName);
+                return objectName;
+            }
+            else
+            {
+                return "ERROR";
+            }
+        }
+
+        /// <param name="client">An initialized Amazon S3 client object.</param>
+        /// <param name="bucketName">The Amazon S3 bucket to which the object
+        /// will be uploaded.</param>
+        /// <param name="objectName">The object to upload.</param>
+        /// <param name="filePath">The path, including file name, of the object
+        /// on the local computer to upload.</param>
+        /// <returns>A boolean value indicating the success or failure of the
+        /// upload procedure.</returns>
+        private static async Task<bool> UploadFileAsync(
+            IAmazonS3 client,
+            string bucketName,
+            string objectName,
+            string filePath,
+            byte[] imageBytes)
+        {
+            var request = new PutObjectRequest
+            {
+                BucketName = bucketName,
+                Key = objectName,
+                FilePath = filePath,
+            };
+            using (var ms = new MemoryStream(imageBytes))
+            {
+                request.InputStream = ms;
+
+                var response = await client.PutObjectAsync(request);
+                if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    Console.WriteLine($"Successfully uploaded {objectName} to {bucketName}.");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine($"Could not upload {objectName} to {bucketName}.");
+                    Console.WriteLine($"{response.ToString}");
+                    return false;
+                }
+            }
         }
 
         public string retrieveS3BucketImageURL(string s3Key)
@@ -38,12 +127,20 @@ namespace MeetGreet.AmazonS3HelperClasses
 
 
         private static IAmazonS3 instantiateS3Client(MySqlConnection connect) {
-            connect.Open();
+            if (connect.State == System.Data.ConnectionState.Open)
+            {
+                Console.WriteLine("The connection state is already open.");
+            }
+            else
+            {
+                connect.Open();
+            }
+
 
             // Sends a request for API KEYS FOR AWS
             MySqlCommand command = new MySqlCommand("SELECT * FROM AWSAPIKey WHERE ID=1", connect);
 
-            IAmazonS3 s3Client = new AmazonS3Client();
+            IAmazonS3 s3Client = new AmazonS3Client("", "", Amazon.RegionEndpoint.USEast1);
 
             MySqlDataReader reader = command.ExecuteReader();
             while (reader.Read())
--- Controllers/IndividualEventController.cs
+++ Controllers/IndividualEventController.cs
@@ -84,7 +84,17 @@ namespace MeetGreet.Controllers
                 _context.SaveChanges();
             }
 
-            await uploadEventImages(imageByteString, userEvent);
+
+            string objectID = await s3Helper.uploadImageToS3Bucket(Convert.FromBase64String(imageByteString), userEvent);
+
+            if(objectID == "ERROR")
+            {
+                Console.Write("ERROR: The image could not be saved to S3. It will not be written to the MYSQL Server.");
+
+            }
+            else {
+                saveImageToDatabase(userEvent, objectID);
+            }
 
             ViewData["Event"] = userEvent;
             ViewData["EventImage"] = new EventImage()
@@ -98,7 +108,7 @@ namespace MeetGreet.Controllers
         private string BUCKET_NAME = "meetgreet-image-store";
 
 
-        private async void saveImageToDatabase(Event userEvent, String s3Key)
+        private void saveImageToDatabase(Event userEvent, String s3Key)
         {
             Image image = new Image()
             {
@@ -107,87 +117,6 @@ namespace MeetGreet.Controllers
             };
             _context.Add(image);
             _context.SaveChanges();
-
-            Console.WriteLine($"TEST PRESIGNED URL: {s3Helper.retrieveS3BucketImageURL(s3Key)}");
         }
-
-
-        private async Task<Boolean> uploadEventImages(string byteString, Event userEvent)
-        {
-            connect.Open();
-            // GET S3 CLIENT FROM SQL
-
-            // Sends a request for API KEYS FOR AWS
-            MySqlCommand command = new MySqlCommand("SELECT * FROM AWSAPIKey WHERE ID=1", connect);
-
-            IAmazonS3 s3Client = new AmazonS3Client();
-
-            // Reads result.
-            MySqlDataReader reader = command.ExecuteReader();
-            while (reader.Read())
-            {
-                s3Client = new AmazonS3Client(reader.GetValue(1).ToString(), reader.GetValue(2).ToString(), Amazon.RegionEndpoint.USEast1);
-            }
-
-            reader.Close();
-
-            string objectName = generateObjectName();
-            byte[] byteArray = Convert.FromBase64String(byteString);
-
-            if(await UploadFileAsync(s3Client, BUCKET_NAME, objectName, "", byteArray))
-            {
-                saveImageToDatabase(userEvent, objectName);
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        private string generateObjectName()
-        {
-            return DateTime.Now.ToString("yyMMdd") + DateTime.Now.ToString("HH:mm:ss") + "-" + Guid.NewGuid().ToString();
-        }
-
-        /// <param name="client">An initialized Amazon S3 client object.</param>
-        /// <param name="bucketName">The Amazon S3 bucket to which the object
-        /// will be uploaded.</param>
-        /// <param name="objectName">The object to upload.</param>
-        /// <param name="filePath">The path, including file name, of the object
-        /// on the local computer to upload.</param>
-        /// <returns>A boolean value indicating the success or failure of the
-        /// upload procedure.</returns>
-        private static async Task<bool> UploadFileAsync(
-            IAmazonS3 client,
-            string bucketName,
-            string objectName,
-            string filePath,
-            byte[] imageBytes)
-        {
-            var request = new PutObjectRequest
-            {
-                BucketName = bucketName,
-                Key = objectName,
-                FilePath = filePath,
-            };
-            using (var ms = new MemoryStream(imageBytes))
-            {
-                request.InputStream = ms;
-
-                var response = await client.PutObjectAsync(request);
-                if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
-                {
-                    Console.WriteLine($"Successfully uploaded {objectName} to {bucketName}.");
-                    return true;
-                }
-                else {
-                    Console.WriteLine($"Could not upload {objectName} to {bucketName}.");
-                    Console.WriteLine($"{response.ToString}");
-                    return false;
-                }
-            }
-        }
-
     }
 }
